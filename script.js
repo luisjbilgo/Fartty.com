a3b10c328e6eb56fa25f8603fa175e708a373e39
Luijo <luisjbilgo2004@gmail.com>
2023-03-26 08:34:30 -0500

    Cambios en la primera pagina frases y equipo en la ultima

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
-import Swup from 'swup';
-const swup = new Swup();
+// Selecciona el elemento span
+const span = document.getElementById('cambiar');
+const span2 = document.getElementById('nuevo');
+
+// Define un arreglo de textos que quieres mostrar
+const textos = ['Experiencias', 'Lugares', 'Amigos'];
+const nuevos = ['Nuevos ', 'Nuevas ', 'Nuevos '];
+
+const colores = ['#96E6ED','#A2F273','#47F3B0','#0FE6C4']
+
+let index = 0;
+
+// Función que actualiza el texto del span
+function cambiarNuevo() {
+  span2.textContent = nuevos[index];
+  index++;
+  if (index >= nuevos.length) {
+    index = 0;
+  }
+}
+
+function cambiarTexto() {
+  span.textContent = textos[index];
+  index++;
+
+  span.style.color = colores[index];
+  
+  if (index >= textos.length) {
+    index = 0;
+  }
+}
+
+ 
+
+
+
+// Cambia el texto cada 2 segundos
+setInterval(cambiarTexto, 2000);
+setInterval(cambiarNuevo, 2000);
